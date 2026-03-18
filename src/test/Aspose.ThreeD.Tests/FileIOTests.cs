3de59f32aa48cc9fccbcec324ecfbbb48b60727d
Bot Lex Chou <bot_lex.chou@aspose.com>
2026-03-18 11:04:02 +0800

    Add FBX 7.4.0 ASCII test file support and test

--- src/test/Aspose.ThreeD.Tests/FileIOTests.cs
+++ src/test/Aspose.ThreeD.Tests/FileIOTests.cs
@@ -578,6 +578,29 @@ namespace Aspose.ThreeD.Tests
         }
 
         [Fact]
+        public void LoadSceneFromFbx7400Ascii_ShouldLoadCorrectly()
+        {
+            var testFile = "/home/lexchou/workspace/aspose/foss.3d.net/testdata/fbx7400ascii/cube.fbx";
+
+            if (!File.Exists(testFile))
+            {
+                throw new FileNotFoundException($"Test file not found: {testFile}");
+            }
+
+            var scene = new Scene();
+            scene.Open(testFile);
+
+            Assert.NotNull(scene);
+            Assert.NotNull(scene.RootNode);
+            Assert.True(scene.RootNode.ChildNodes.Count > 0);
+
+            var node = scene.RootNode.ChildNodes[0];
+            Assert.Equal("Model::pCube1", node.Name);
+            Assert.True(node.Entities.Count > 0);
+            Assert.NotNull(node.Entities[0] as Mesh);
+        }
+
+        [Fact]
         public void LoadSceneFromPlyBinary_ShouldLoadCorrectly()
         {
             var testFile = "/home/lexchou/workspace/aspose/foss.3d.net/testdata/input/cube_binary.ply";
