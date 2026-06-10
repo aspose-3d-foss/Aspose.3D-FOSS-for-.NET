7d3211e7136a19bf9ab47807519e994382b73e26
Bot Lex Chou <bot_lex.chou@aspose.com>
2026-06-10 14:01:39 +0800

    fix: Update AnimationNode and BindPoint to match On-Premise 26.2.0
    - AnimationNode: Now inherits from A3DObject, implements INamedObject
      - Added BindPoints (IList<BindPoint>)
      - Added SubAnimations (IList<AnimationNode>)
      - Added FindBindPoint, GetBindPoint, GetKeyframeSequence, CreateBindPoint methods
    
    - BindPoint: Now inherits from A3DObject, implements INamedObject
      - Added Property property
      - Added ChannelsCount property
      - Added GetKeyframeSequence, CreateKeyframeSequence, BindKeyframeSequence methods
      - Added AddChannel overloads, GetChannel, ResetChannels methods
      - Added indexer for AnimationChannel
    
    All 63 tests pass. Build succeeds with 0 errors, 0 warnings.

--- src/main/Aspose.ThreeD/Aspose/ThreeD/Animation/AnimationNode.cs
+++ src/main/Aspose.ThreeD/Aspose/ThreeD/Animation/AnimationNode.cs
@@ -3,66 +3,100 @@ using Aspose.ThreeD.Utilities;
 
 namespace Aspose.ThreeD.Animation
 {
-    public class AnimationNode
+    /// <summary>
+    /// Aspose.3D's supports animation hierarchy, each animation can be composed by several animations and animation's key-frame definition.
+    /// </summary>
+    public class AnimationNode : A3DObject
     {
-        private List<AnimationNode> _children;
-        private List<AnimationChannel> _channels;
-        private string _name;
-        private Matrix4 _localMatrix;
-        private Matrix4 _worldMatrix;
+        private List<BindPoint> _bindPoints;
+        private List<AnimationNode> _subAnimations;
 
+        /// <summary>
+        /// Initializes a new instance of the AnimationNode class.
+        /// </summary>
         public AnimationNode()
         {
-            _children = new List<AnimationNode>();
-            _channels = new List<AnimationChannel>();
-            _name = string.Empty;
-            _localMatrix = Matrix4.Identity;
-            _worldMatrix = Matrix4.Identity;
+            _bindPoints = new List<BindPoint>();
+            _subAnimations = new List<AnimationNode>();
         }
 
-        public AnimationNode(string name)
+        /// <summary>
+        /// Initializes a new instance of the AnimationNode class.
+        /// </summary>
+        /// <param name="name">The name of the animation node.</param>
+        public AnimationNode(string name) : base(name)
         {
-            _children = new List<AnimationNode>();
-            _channels = new List<AnimationChannel>();
-            _name = name;
-            _localMatrix = Matrix4.Identity;
-            _worldMatrix = Matrix4.Identity;
+            _bindPoints = new List<BindPoint>();
+            _subAnimations = new List<AnimationNode>();
         }
 
-        public string Name
+        /// <summary>
+        /// Gets the current property bind points.
+        /// </summary>
+        public IList<BindPoint> BindPoints => _bindPoints;
+
+        /// <summary>
+        /// Gets the sub-animation nodes under current animations.
+        /// </summary>
+        public IList<AnimationNode> SubAnimations => _subAnimations;
+
+        /// <summary>
+        /// Finds the bind point by target and name.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <param name="name">The name of the bind point.</param>
+        /// <returns>The found bind point, or null if not found.</returns>
+        public BindPoint FindBindPoint(A3DObject target, string name)
         {
-            get => _name;
-            set => _name = value;
+            return null!;
         }
 
-        public List<AnimationNode> Children => _children;
-
-        public List<AnimationChannel> Channels => _channels;
-
-        public void AddChild(AnimationNode node)
+        /// <summary>
+        /// Gets the animation bind point on given property.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <param name="propName">The property name.</param>
+        /// <param name="create">Whether to create the bind point if not exists.</param>
+        /// <returns>The bind point.</returns>
+        public BindPoint GetBindPoint(A3DObject target, string propName, bool create)
         {
-            if (node != null)
-            {
-                _children.Add(node);
-            }
+            return null!;
         }
 
-        public void AddChannel(AnimationChannel channel)
+        /// <summary>
+        /// Gets the keyframe sequence on given property and channel.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <param name="propName">The property name.</param>
+        /// <param name="channelName">The channel name.</param>
+        /// <param name="create">Whether to create the sequence if not exists.</param>
+        /// <returns>The keyframe sequence.</returns>
+        public KeyframeSequence GetKeyframeSequence(A3DObject target, string propName, string channelName, bool create)
         {
-            if (channel != null)
-            {
-                _channels.Add(channel);
-            }
+            return null!;
         }
 
-        public Matrix4 GetLocalMatrix()
+        /// <summary>
+        /// Gets the keyframe sequence on given property.
+        /// </summary>
+        /// <param name="target">The target object.</param>
+        /// <param name="propName">The property name.</param>
+        /// <param name="create">Whether to create the sequence if not exists.</param>
+        /// <returns>The keyframe sequence.</returns>
+        public KeyframeSequence GetKeyframeSequence(A3DObject target, string propName, bool create)
         {
-            return _localMatrix;
+            return null!;
         }
 
-        public Matrix4 GetWorldMatrix()
+        /// <summary>
+        /// Creates a BindPoint based on the property data type.
+        /// </summary>
+        /// <param name="obj">The target object.</param>
+        /// <param name="propName">The property name.</param>
+        /// <returns>The created bind point.</returns>
+        public BindPoint CreateBindPoint(A3DObject obj, string propName)
         {
-            return _worldMatrix;
+            return null!;
         }
     }
 }
--- src/main/Aspose.ThreeD/Aspose/ThreeD/Animation/BindPoint.cs
+++ src/main/Aspose.ThreeD/Aspose/ThreeD/Animation/BindPoint.cs
@@ -1,41 +1,136 @@
 using System;
+using System.Collections.Generic;
 
 namespace Aspose.ThreeD.Animation
 {
-    public class BindPoint
+    /// <summary>
+    /// A BindPoint is usually created on an object's property, some property types contains multiple component fields(like a Vector3 field),
+    /// will generate channel for each component field and connects the field to one or more keyframe sequence instance(s) through the channels.
+    /// </summary>
+    public class BindPoint : A3DObject
     {
-        private string _target;
-        private string _propertyName;
-        private AnimationChannel[] _channels;
+        private Property _property;
+        private List<AnimationChannel> _channels;
+        private Dictionary<string, AnimationChannel> _channelMap;
 
-        public BindPoint(string target, string propertyName)
+        /// <summary>
+        /// Initializes a new instance of the BindPoint class.
+        /// </summary>
+        /// <param name="scene">The scene.</param>
+        /// <param name="prop">The property associated with this bind point.</param>
+        public BindPoint(Scene scene, Property prop)
         {
-            _target = target;
-            _propertyName = propertyName;
-            _channels = new AnimationChannel[0];
+            _property = prop;
+            _channels = new List<AnimationChannel>();
+            _channelMap = new Dictionary<string, AnimationChannel>();
         }
 
-        public string Target
+        /// <summary>
+        /// Gets the property associated with the CurveMapping.
+        /// </summary>
+        public Property Property => _property;
+
+        /// <summary>
+        /// Gets the animation channel by item index.
+        /// </summary>
+        /// <param name="index">The channel index.</param>
+        /// <returns>The animation channel.</returns>
+        public AnimationChannel this[int index] => _channels[index];
+
+        /// <summary>
+        /// Gets the total number of property channels defined in this animation curve mapping.
+        /// </summary>
+        public int ChannelsCount => _channels.Count;
+
+        /// <summary>
+        /// Gets the first keyframe sequence in specified channel.
+        /// </summary>
+        /// <param name="channelName">The channel name.</param>
+        /// <returns>The keyframe sequence.</returns>
+        public KeyframeSequence GetKeyframeSequence(string channelName)
         {
-            get => _target;
-            set => _target = value;
+            return null!;
         }
 
-        public string PropertyName
+        /// <summary>
+        /// Creates a new curve and connects it to the first channel of the curve mapping.
+        /// </summary>
+        /// <param name="name">The name of the keyframe sequence.</param>
+        /// <returns>The created keyframe sequence.</returns>
+        public KeyframeSequence CreateKeyframeSequence(string name)
         {
-            get => _propertyName;
-            set => _propertyName = value;
+            return null!;
         }
 
-        public AnimationChannel[] Channels
+        /// <summary>
+        /// Bind the keyframe sequence to specified channel.
+        /// </summary>
+        /// <param name="channelName">The channel name.</param>
+        /// <param name="sequence">The keyframe sequence to bind.</param>
+        public void BindKeyframeSequence(string channelName, KeyframeSequence sequence)
         {
-            get => _channels;
-            set => _channels = value;
         }
 
-        public object GetProperty()
+        /// <summary>
+        /// Gets channel by given name.
+        /// </summary>
+        /// <param name="channelName">The channel name.</param>
+        /// <returns>The animation channel.</returns>
+        public AnimationChannel GetChannel(string channelName)
         {
-            return null;
+            return null!;
+        }
+
+        /// <summary>
+        /// Adds the specified channel property.
+        /// </summary>
+        /// <param name="name">The channel name.</param>
+        /// <param name="value">The channel value.</param>
+        /// <returns>True if the channel was added successfully.</returns>
+        public bool AddChannel<T>(string name, T value)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Adds the specified channel property.
+        /// </summary>
+        /// <param name="name">The channel name.</param>
+        /// <param name="value">The channel value.</param>
+        /// <returns>True if the channel was added successfully.</returns>
+        public bool AddChannel(string name, object value)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Adds the specified channel property.
+        /// </summary>
+        /// <param name="name">The channel name.</param>
+        /// <param name="type">The channel type.</param>
+        /// <param name="value">The channel value.</param>
+        /// <returns>True if the channel was added successfully.</returns>
+        public bool AddChannel(string name, Type type, object value)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Empties the property channels of this animation curve mapping.
+        /// </summary>
+        public void ResetChannels()
+        {
+            _channels.Clear();
+            _channelMap.Clear();
+        }
+
+        /// <summary>
+        /// Formats object to string.
+        /// </summary>
+        /// <returns>The string representation of this bind point.</returns>
+        public override string ToString()
+        {
+            return $"BindPoint: {Name}";
         }
     }
 }
